416d5af7caa09e2d87b286a7c51f42a97e63b041
Vikram Verma <git@vikramverma.com>
2013-08-20 18:47:26 +0800

    export sandbox; test this is so

--- test/sandbox_spec.js
+++ test/sandbox_spec.js
@@ -1,7 +1,7 @@
 var sandbox = require('sandbox');
 
-describe("test", function() {
-  it("tests expectations", function() {
-    expect(true).toBe(true);
+describe("sandbox", function() {
+  it("should export the sandbox function", function() {
+    expect(typeof(sandbox)).toBe('function');
   });
 });
